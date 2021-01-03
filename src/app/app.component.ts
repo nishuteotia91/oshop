4714f9823dc52141d3580b8728b8260e2fc83f0e
nishuteotia91 <nishuteotia91@gmail.com>
2021-01-03 18:52:45 -0500

    changes for shopping cart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ import { Router } from '@angular/router';
 export class AppComponent {
  constructor(private userService: UserService, private auth: AuthService,router: Router){
 auth.user$.subscribe(user => {
+  //hello
   if (user) {
     userService.save(user);
    let returnUrl = localStorage.getItem('returnUrl');
